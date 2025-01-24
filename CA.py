bfa52dc47329bdd444ba9b16f6393c792cb80959
Fabian Ivulic <96046458+fabianivulic@users.noreply.github.com>
2025-01-24 12:51:43 +0100

    Tumor spawning added

--- CA.py
+++ CA.py
@@ -21,6 +21,22 @@ def initialize_background(size):
                 background[x, y] = np.exp(-distance**2 / (2 * sigma**2))
     return background
 
+def create_tumor(size, background, tumor_prob, tumor_factor):
+    """Creates the list of coordinates for the tumor."""
+    tumor = []
+    center = size // 2
+    empty_radius = size / 10
+    tumor_radius = size / 8
+
+    for x in range(size):
+        for y in range(size):
+            distance = np.sqrt((x - center) ** 2 + (y - center) ** 2)
+            if tumor_radius > distance > empty_radius and random.random() > tumor_prob:
+                tumor.append((x, y))
+                background[x, y] += tumor_factor
+
+    return tumor
+
 def get_neighbors(x, y, size, wrap_around=True):
     """Return neighboring positions with periodic boundaries if enabled."""
     neighbors = []
@@ -111,6 +127,8 @@ def simulate_CA_with_entropy(size=200, num_seeds=10, steps=1000, bias_factor=0.9
     seeds = [(random.choice([0, size-1]), random.randint(0, size-1)) if random.random() < 0.5 else (random.randint(0, size-1), random.choice([0, size-1])) for _ in range(num_seeds)]
     occupied.update(seeds)
     
+    tumor = create_tumor(size, background, tumor_prob=0.5, tumor_factor=10)
+
     entropies = []  # To store entropy values over time
     
     for _ in range(steps):
@@ -124,6 +142,8 @@ def simulate_CA_with_entropy(size=200, num_seeds=10, steps=1000, bias_factor=0.9
         
         # Create a binary grid (1 for occupied, 0 for unoccupied)
         grid = np.zeros((size, size))
+        for x, y in tumor:
+            grid[x, y] = 2
         for x, y in occupied:
             grid[x, y] = 1
         
@@ -135,7 +155,7 @@ def simulate_CA_with_entropy(size=200, num_seeds=10, steps=1000, bias_factor=0.9
     plt.figure(figsize=(10, 5))
     
     from matplotlib.colors import ListedColormap
-    cmap = ListedColormap(["white", "red"])
+    cmap = ListedColormap(["white", "red", "green"])
 
     # Plot the growth
     plt.subplot(1, 2, 1)
